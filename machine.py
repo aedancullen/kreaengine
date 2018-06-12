23c7e0b83c43d71d301a01088214c8d193830767
Aedan Cullen <aedancullen@gmail.com>
2018-06-12 11:46:34 -0400

    Add module ref. keeping and update() to machine,py

--- machine.py
+++ machine.py
@@ -6,6 +6,7 @@
 import irload
 import ctypes as c
 
+KMACHINE_DEFAULT_FN = "default_machine.ll"
 KMACHINE_IR_FUNCS = [
     ("1",),
     ("2",),
@@ -13,8 +14,18 @@ KMACHINE_IR_FUNCS = [
 
 class KreaLLMachine:
 
-    def __init__(self, ir):
-        bound_funcs = irload.bind_funcs_from_module(
+    def __init__(self, ir=None):
+        self.current_module = None
+        
+        if ir is None: ir = irload.read_module_from_file(KMACHINE_DEFAULT_FN)
+        self.update(ir)
+        
+    def update(self, ir):
+        if self.current_module is not None:
+            irload.engine.remove_module(self.current_module)
+            
+        module, bound_funcs = irload.bind_funcs_from_module(
             ir, KMACHINE_IR_FUNCS
         )
         self.__dict__.update(bound_funcs)
+        self.current_module = module
