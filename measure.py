33c3ec5883bf68cfba052221658af56eebbe1503
Aedan Cullen <aedancullen@gmail.com>
2018-06-12 11:47:45 -0400

    Add module ref. keeping and update() to measure.py

--- measure.py
+++ measure.py
@@ -6,6 +6,7 @@
 import irload
 import ctypes as c
 
+KMEASURE_DEFAULT_FN = "default_measure.ll"
 KMEASURE_IR_FUNCS = [
     ("1",),
     ("2",),
@@ -13,8 +14,18 @@ KMEASURE_IR_FUNCS = [
 
 class KreaLLMeasure:
 
-    def __init__(self, ir):
-        bound_funcs = irload.bind_funcs_from_module(
+    def __init__(self, ir=None):
+        self.current_module = None
+        
+        if ir is None: ir = irload.read_module_from_file(KMEASURE_DEFAULT_FN)
+        self.update(ir)
+        
+    def update(self, ir):
+        if self.current_module is not None:
+            irload.engine.remove_module(self.current_module)
+            
+        module, bound_funcs = irload.bind_funcs_from_module(
             ir, KMEASURE_IR_FUNCS
         )
         self.__dict__.update(bound_funcs)
+        self.current_module = module
