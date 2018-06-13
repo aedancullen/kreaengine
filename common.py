14050e912c1913fe7230f1ed228382b7e29dd46d
Aedan Cullen <aedancullen@gmail.com>
2018-06-13 09:25:55 -0400

    Reduce to sync-func only in common.py

--- common.py
+++ common.py
@@ -14,15 +14,6 @@ PS_PORT = 17100
 
 PSIF_REG_STR = "kreaengine_psif"
 PSIF_SYNC_FUNC = "sync"
-PSIF_CTRL_FUNCS = [
-    "assign_measureir",
-    "assign_machineir",
-    "assign_machineset",
-    "assign_optimizeset",
-    "assign_mode_recurrent",
-    "enable_sync",
-    "disable_sync"
-]
 
 _psw = None
 
@@ -33,43 +24,29 @@ ServerParamSyncManager.register(PSIF_REG_STR, callable=lambda:_psw)
 ClientParamSyncManager.register(PSIF_REG_STR)
 
 
-_hostmanager = None
-_workermanager = None
-_ctrlmanager = None
+_manager = None
 
 
 def parallel_boostrap_host(instance, authkey):
-    global _hostmanager, _psw
+    global _manager, _psw
 
     _psw = instance
-    _hostmanager = ServerParamSyncManager(address=("", PS_PORT), authkey=authkey)
-    _hostmanager.start()
+    _manager = ServerParamSyncManager(address=("", PS_PORT), authkey=authkey)
+    _manager.start()
     
 def parallel_boostrap_worker(addrstr, authkey):
     pass
 
 
 def bind_sync(addrstr, authkey):
-    global _workermanager
+    global _manager
 
-    _workermanager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
-    _workermanager.connect()
-    syncif = getattr(_workermanager, PSIF_REG_STR)()
+    _manager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
+    _manager.connect()
+    syncif = getattr(_manager, PSIF_REG_STR)()
     
     return getattr(syncif, PSIF_SYNC_FUNC)
 
-def bind_ctrl(addrstr, authkey):
-    global _ctrlmanager
-    
-    _ctrlmanager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
-    _ctrlmanager.connect()
-    syncif = getattr(_ctrlmanager, PSIF_REG_STR)()
-    
-    bound_funcs = {}
-    for func in PSIF_CTRL_FUNCS:
-        bound_funcs[func] = getattr(syncif, func)
-        
-    return bound_funcs
 
 class ParamSet:
     pass
