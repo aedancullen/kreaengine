66130bdefb8609968a94d0cc319fb6b3559e9987
Aedan Cullen <aedancullen@gmail.com>
2018-06-14 19:45:34 -0400

    Revert "Reduce to sync-func only in common.py"
    This reverts commit 14050e912c1913fe7230f1ed228382b7e29dd46d.

--- common.py
+++ common.py
@@ -14,6 +14,15 @@ PS_PORT = 17100
 
 PSIF_REG_STR = "kreaengine_psif"
 PSIF_SYNC_FUNC = "sync"
+PSIF_CTRL_FUNCS = [
+    "assign_measureir",
+    "assign_machineir",
+    "assign_machineset",
+    "assign_optimizeset",
+    "assign_mode_recurrent",
+    "enable_sync",
+    "disable_sync"
+]
 
 _psw = None
 
@@ -24,29 +33,43 @@ ServerParamSyncManager.register(PSIF_REG_STR, callable=lambda:_psw)
 ClientParamSyncManager.register(PSIF_REG_STR)
 
 
-_manager = None
+_hostmanager = None
+_workermanager = None
+_ctrlmanager = None
 
 
 def parallel_boostrap_host(instance, authkey):
-    global _manager, _psw
+    global _hostmanager, _psw
 
     _psw = instance
-    _manager = ServerParamSyncManager(address=("", PS_PORT), authkey=authkey)
-    _manager.start()
+    _hostmanager = ServerParamSyncManager(address=("", PS_PORT), authkey=authkey)
+    _hostmanager.start()
     
 def parallel_boostrap_worker(addrstr, authkey):
     pass
 
 
 def bind_sync(addrstr, authkey):
-    global _manager
+    global _workermanager
 
-    _manager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
-    _manager.connect()
-    syncif = getattr(_manager, PSIF_REG_STR)()
+    _workermanager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
+    _workermanager.connect()
+    syncif = getattr(_workermanager, PSIF_REG_STR)()
     
     return getattr(syncif, PSIF_SYNC_FUNC)
 
+def bind_ctrl(addrstr, authkey):
+    global _ctrlmanager
+    
+    _ctrlmanager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
+    _ctrlmanager.connect()
+    syncif = getattr(_ctrlmanager, PSIF_REG_STR)()
+    
+    bound_funcs = {}
+    for func in PSIF_CTRL_FUNCS:
+        bound_funcs[func] = getattr(syncif, func)
+        
+    return bound_funcs
 
 class ParamSet:
     pass
