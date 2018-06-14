ebdffce51b8eaed6b529f3fa79594a7964f23c46
Aedan Cullen <aedancullen@gmail.com>
2018-06-14 19:45:41 -0400

    Revert "Add new connection format to common.py"
    This reverts commit c45bed546ba0b0fabe939ee1417dba1207ed5b12.

--- common.py
+++ common.py
@@ -13,63 +13,43 @@ class NoHostRetryException(Exception): pass
 PS_PORT = 17100
 
 PSIF_REG_STR = "kreaengine_psif"
-PSIF_SYNC_FUNC = "sync"
-PSIF_CTRL_FUNCS = [
-    "assign_measureir",
-    "assign_machineir",
-    "assign_machineset",
-    "assign_optimizeset",
-    "assign_mode_recurrent",
-    "enable_sync",
-    "disable_sync"
-]
+PSIF_SYNC_FUNC = "kreaengine_sync"
 
-_psw = None
 
-class ServerParamSyncManager(managers.BaseManager): pass
-class ClientParamSyncManager(managers.BaseManager): pass
+_host_sync_callable = lambda *args,**kwargs: None
 
-ServerParamSyncManager.register(PSIF_REG_STR, callable=lambda:_psw)
-ClientParamSyncManager.register(PSIF_REG_STR)
+
+class ParamSyncInterface: pass
+
+_psw = ParamSyncInterface()
+setattr(_psw, PSIF_SYNC_FUNC, lambda *args,**kwargs:_host_sync_callable(*args,**kwargs))
+
+
+class HostParamSyncManager(managers.BaseManager): pass
+class WorkerParamSyncManager(managers.BaseManager): pass
+
+HostParamSyncManager.register(PSIF_REG_STR, callable=lambda:_psw)
+WorkerParamSyncManager.register(PSIF_REG_STR)
 
 
 _hostmanager = None
 _workermanager = None
-_ctrlmanager = None
 
+def host_startsync(host_sync_callable, authkey):
+    global _host_sync_callable, _hostmanager
 
-def parallel_boostrap_host(instance, authkey):
-    global _hostmanager, _psw
-
-    _psw = instance
-    _hostmanager = ServerParamSyncManager(address=("", PS_PORT), authkey=authkey)
+    _host_sync_callable = host_sync_callable
+    _hostmanager = HostParamSyncManager(address=("", PS_PORT), authkey=authkey)
     _hostmanager.start()
-    
-def parallel_boostrap_worker(addrstr, authkey):
-    pass
 
-
-def bind_sync(addrstr, authkey):
+def worker_getsync(addrstr, authkey):
     global _workermanager
 
-    _workermanager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
+    _workermanager = WorkerParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
     _workermanager.connect()
     syncif = getattr(_workermanager, PSIF_REG_STR)()
-    
     return getattr(syncif, PSIF_SYNC_FUNC)
 
-def bind_ctrl(addrstr, authkey):
-    global _ctrlmanager
-    
-    _ctrlmanager = ClientParamSyncManager(address=(addrstr, PS_PORT), authkey=authkey)
-    _ctrlmanager.connect()
-    syncif = getattr(_ctrlmanager, PSIF_REG_STR)()
-    
-    bound_funcs = {}
-    for func in PSIF_CTRL_FUNCS:
-        bound_funcs[func] = getattr(syncif, func)
-        
-    return bound_funcs
 
 class ParamSet:
     pass
